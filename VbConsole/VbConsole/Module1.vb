276101855f45390d9f4b46ab1e5f646c8973ad79
clebermarques1984 <clebermarques1984@gmail.com>
2017-04-24 10:29:52 -0300

    Teste de SoapRequest

---
+++ VbConsole/VbConsole/Module1.vb
@@ -0,0 +1,78 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Xml
+
+Module Module1
+	Sub Main()
+		Dim retorno As String = GetResponse("http://200.152.194.6:8888/consultas.asmx",
+									  "http://tempuri.org/ConsultaConsolidadaPagamentoPorData",
+									  My.Resources.App.SOAPEnvelope)
+		Console.WriteLine(retorno)
+		Console.Read()
+	End Sub
+
+	''' <summary>
+	''' Create a soap webrequest to [Url]
+	''' </summary>
+	''' <returns></returns>
+	Public Function CreateWebRequest(url As String, soapAction As String) As HttpWebRequest
+		Dim request As HttpWebRequest = WebRequest.Create(url)
+		request.Headers.Add($"SOAPAction:{soapAction}")
+		request.ContentType = "text/xml; charset=utf-8"
+		request.Accept = "text/xml"
+		request.Method = "POST"
+		Return request
+	End Function
+
+	Public Function GetResponse(url As String, soapAction As String, soapEnvelope As String) As String
+		Try
+			Dim request As HttpWebRequest = CreateWebRequest(url, soapAction)
+			Dim soapEnvelopeXml As New XmlDocument()
+			soapEnvelopeXml.LoadXml(My.Resources.App.SOAPEnvelope)
+			Using stream As Stream = request.GetRequestStream
+				soapEnvelopeXml.Save(stream)
+			End Using
+			Using response As WebResponse = request.GetResponse()
+				Using rd As New StreamReader(response.GetResponseStream())
+					Dim soapResult As String = rd.ReadToEnd()
+					Return soapResult
+				End Using
+			End Using
+		Catch ex As Exception
+			Return ex.Message
+		End Try
+	End Function
+End Module
+
+
+Public Class Entity
+	Private cmd As String
+
+	Private Shared _instance As Entity
+
+	Private Sub New(tableName As String)
+		cmd = $"select * from {tableName}"
+	End Sub
+
+	Public Shared Function Create(tableName As String) As Entity
+		If _instance Is Nothing Then
+			Return New Entity(tableName)
+		Else
+			Return _instance
+		End If
+	End Function
+
+	Public Function Where(predicate As String) As Entity
+		cmd += $" where {predicate}"
+		Return Me
+	End Function
+
+	Public Function Order(collumns() As String) As Entity
+		cmd += $" order by {String.Join(",", collumns)}"
+		Return Me
+	End Function
+
+	Public Function GetCmdText() As String
+		Return cmd
+	End Function
+End Class
---
+++ VbConsole/VbConsole/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("VbConsole")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Microsoft")>
+<Assembly: AssemblyProduct("VbConsole")>
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("40469fe3-3586-4860-b2de-3ac3f82a3741")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
